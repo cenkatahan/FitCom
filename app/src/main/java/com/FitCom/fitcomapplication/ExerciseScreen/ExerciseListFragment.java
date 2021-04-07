90de5b1374c7961f56f07289be4695807f0c0e9d
Cenk Atahan Özbek <atahanozbek@outlook.com.tr>
2021-04-07 17:30:38 +0300

    exersice v1

---
+++ app/src/main/java/com/FitCom/fitcomapplication/ExerciseScreen/ExerciseListFragment.java
@@ -0,0 +1,41 @@
+package com.FitCom.fitcomapplication.ExerciseScreen;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.FitCom.fitcomapplication.R;
+
+
+public class ExerciseListFragment extends Fragment {
+
+
+    public ExerciseListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exercise_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    
+}
