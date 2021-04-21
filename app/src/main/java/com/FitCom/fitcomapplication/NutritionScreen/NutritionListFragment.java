103d11c0ef7a07e92b8b0cb4aa5895fe197418f3
Cenk Atahan Özbek <atahanozbek@outlook.com.tr>
2021-04-21 20:09:26 +0300

    all nutrition page progress done

--- app/src/main/java/com/FitCom/fitcomapplication/NutritionScreen/NutritionListFragment.java
+++ app/src/main/java/com/FitCom/fitcomapplication/NutritionScreen/NutritionListFragment.java
@@ -4,13 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.FitCom.fitcomapplication.R;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 
 public class NutritionListFragment extends Fragment {
+
+    private FirebaseFirestore firebaseFirestore;
+    private ArrayList<String> names, calories, carbs, proteins, fats;
+    private RecAdaptorNutrition recAdaptorNutrition;
+    private RecyclerView recyclerView;
 
 
     public NutritionListFragment() {}
@@ -26,5 +45,66 @@ public class NutritionListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_nutrition_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        names = new ArrayList<>();
+        calories = new ArrayList<>();
+        carbs = new ArrayList<>();
+        proteins = new ArrayList<>();
+        fats = new ArrayList<>();
+
+
+
+        recAdaptorNutrition = new RecAdaptorNutrition(names, calories, carbs, proteins, fats);
+        recyclerView = view.findViewById(R.id.recycler_nutrition_list);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(recAdaptorNutrition);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        fetchDataFromFB();
+    }
+
+    private void fetchDataFromFB(){
+        CollectionReference collectionReference = firebaseFirestore.collection("Nutrition");
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null){
+                    Toast.makeText(getContext(), error.getLocalizedMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+
+                if(value != null){
+                    for (DocumentSnapshot snapshot : value.getDocuments()) {
+
+                        Map<String, Object> data = snapshot.getData();
+
+                        String name = (String) data.get("name");
+                        String calorie = (String) data.get("Calorie");
+                        String carb = (String) data.get("Carb");
+                        String protein = (String) data.get("Protein");
+                        String fat = (String) data.get("Fat");
+
+                        names.add(name);
+                        calories.add(calorie);
+                        carbs.add(carb);
+                        proteins.add(protein);
+                        fats.add(fat);
+
+                        recAdaptorNutrition.notifyDataSetChanged();
+
+                    }
+                    
+                }
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/FitCom/fitcomapplication/NutritionScreen/RecAdaptorNutrition.java
+++ app/src/main/java/com/FitCom/fitcomapplication/NutritionScreen/RecAdaptorNutrition.java
@@ -1,34 +1,71 @@
 package com.FitCom.fitcomapplication.NutritionScreen;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.FitCom.fitcomapplication.R;
+
+import java.util.ArrayList;
+
 public class RecAdaptorNutrition extends RecyclerView.Adapter<RecAdaptorNutrition.NutritionPlaceHolder> {
 
+    private ArrayList<String> nutrition_names;
+    private ArrayList<String> nutrition_calories;
+    private ArrayList<String> nutrition_carbs;
+    private ArrayList<String> nutrition_protein;
+    private ArrayList<String> nutrition_fat;
+
+    public RecAdaptorNutrition(ArrayList<String> nutrition_names, ArrayList<String> nutrition_calories,
+                               ArrayList<String> nutrition_carbs, ArrayList<String> nutrition_protein, ArrayList<String> nutrition_fat) {
+
+        this.nutrition_names = nutrition_names;
+        this.nutrition_calories = nutrition_calories;
+        this.nutrition_carbs = nutrition_carbs;
+        this.nutrition_protein = nutrition_protein;
+        this.nutrition_fat = nutrition_fat;
+    }
 
     @NonNull
     @Override
     public NutritionPlaceHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.recycler_row_nutrition, parent, false);
+        return new NutritionPlaceHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull NutritionPlaceHolder holder, int position) {
-
+        holder.names.setText(nutrition_names.get(position));
+        holder.calorie.setText(nutrition_calories.get(position));
+        holder.carb.setText(nutrition_carbs.get(position));
+        holder.protein.setText(nutrition_protein.get(position));
+        holder.fat.setText(nutrition_fat.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return nutrition_names.size();
     }
 
     class NutritionPlaceHolder extends RecyclerView.ViewHolder{
 
+        private TextView names, calorie, carb, protein, fat;
+
         public NutritionPlaceHolder(@NonNull View itemView) {
             super(itemView);
+
+            names = itemView.findViewById(R.id.row_nutr_name);
+            calorie = itemView.findViewById(R.id.row_nutr_calorie);
+            carb = itemView.findViewById(R.id.row_nutr_carb);
+            protein = itemView.findViewById(R.id.row_nutr_protein);
+            fat = itemView.findViewById(R.id.row_nutr_fat);
+
         }
     }
 }
