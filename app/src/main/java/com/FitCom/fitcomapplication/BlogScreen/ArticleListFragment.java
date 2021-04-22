1e7622a3041ebe9d2665dafd330fe0b31faab172
Cenk Atahan Özbek <atahanozbek@outlook.com.tr>
2021-04-22 19:19:50 +0300

    blog screen finished

---
+++ app/src/main/java/com/FitCom/fitcomapplication/BlogScreen/ArticleListFragment.java
@@ -0,0 +1,94 @@
+package com.FitCom.fitcomapplication.BlogScreen;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.FitCom.fitcomapplication.R;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class ArticleListFragment extends Fragment {
+
+    private FirebaseFirestore firebaseFirestore;
+    private RecAdaptorBlog recAdaptorBlog;
+    private ArrayList<String> titles;
+    private RecyclerView recyclerView;
+
+    public ArticleListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_article_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        titles = new ArrayList<>();
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        recyclerView = view.findViewById(R.id.recycler_blog_list);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recAdaptorBlog = new RecAdaptorBlog(titles);
+        recyclerView.setAdapter(recAdaptorBlog);
+
+        fetchFromFB();
+
+    }
+
+
+
+    private void fetchFromFB(){
+        CollectionReference collectionReference = firebaseFirestore.collection("Article");
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if(error != null) {
+                    Toast.makeText(getContext(), error.getLocalizedMessage().toString(), Toast.LENGTH_LONG).show();
+                }
+
+                if(value != null){
+                    for (DocumentSnapshot snapshot: value.getDocuments()){
+                        Map<String, Object> data = snapshot.getData();
+
+                        String title = (String) data.get("title");
+
+                        titles.add(title);
+
+                        recAdaptorBlog.notifyDataSetChanged();
+                    }
+                }
+
+            }
+        });
+    }
+}
