42eace0cb81eafddeb84c3961e106d5a93874dc9
Osman Can Çınar <osmancancinar@gmail.com>
2021-05-31 00:37:09 +0300

    list indexes are fixed.

--- app/src/main/java/com/FitCom/fitcomapplication/ExerciseScreen/ExerciseListFragment.java
+++ app/src/main/java/com/FitCom/fitcomapplication/ExerciseScreen/ExerciseListFragment.java
@@ -16,6 +16,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.Map;
@@ -59,7 +60,7 @@ public class ExerciseListFragment extends Fragment {
 
     public void fetchDataFromFB(){
         CollectionReference collectionReference = firebaseFirestore.collection("Exercises");
-        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+        collectionReference.orderBy("id", Query.Direction.ASCENDING).addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
                 if(error != null) {
